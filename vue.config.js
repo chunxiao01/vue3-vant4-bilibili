5512cc01fc1ee9517d76a892b13155c622e97431
chunxiao01 <chunxiao01@outlook.com>
2022-05-10 19:22:37 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require("@vue/cli-service");
+const path = require("path")
+// function resolve(dir) {
+//   return path.join(__dirname, dir);
+// }
+const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
-});
+  runtimeCompiler: true, //开启运行时编译
+  chainWebpack: (config) => {
+    //配置别名
+    config.resolve.alias
+      .set("@", path.resolve(__dirname, "src"))
+      .set("assets", "@/assets")
+      .set("common", "@/common")
+      .set("components", "@/components")
+      .set("network", "@/network")
+      .set("views", "@/views")
+  }
+})
